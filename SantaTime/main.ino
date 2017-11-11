1f9e4b856c2e90a317d2bf5aeda3d5860958831c
Joshua Johnson <joshua@weareenvoy.com>
2017-11-11 05:22:48 -0800

    Added support for Christmas Tree blinking via relay. Updated LEDs to be on Digital GPIO bands and use digital logic.

--- SantaTime/main.ino
+++ SantaTime/main.ino
@@ -1,36 +1,44 @@
 #define relayPin D0
-#define sadFaceLED A1
-#define happyFaceLED A2
-#define mehFaceLED A0
+#define sadFaceLED D1
+#define happyFaceLED D2
+#define mehFaceLED D3
 #define nightTimeThreshold 400
 #define CommandSuccessful 1
 #define CommandFailed 0
+#define flashDelayInMs = 2000;
 
 signed int relayState = LOW;
+signed int globalTimer = 0;
+bool shouldFlashLights = false;
 
 void setup() {
   pinMode(relayPin, OUTPUT);
   pinMode(sadFaceLED, OUTPUT);
   pinMode(happyFaceLED, OUTPUT);
   pinMode(mehFaceLED, OUTPUT);
+  // All Lights Start off
+  digitalWrite(sadFaceLED, LOW);
+  digitalWrite(mehFaceLED, LOW);
+  digitalWrite(happyFaceLED, LOW);
+  // Christmas Lights controlled through Relay should start off
   digitalWrite(relayPin, LOW);
   Particle.function("santasMood", setSantasMood);
   Particle.function("relayControl", setRelayMode);
+  Serial.begin(9600);
 }
 
 int setRelayMode(String command) {
   char relaySetting = command[0];
   switch (relaySetting) {
     case '0':
-      if (relayState == HIGH) {
+        shouldFlashLights = false;
         relayState = LOW;
         digitalWrite(relayPin, LOW);
-      }
+        return CommandSuccessful;
     case '1':
-      if (relayState == LOW) {
-          relayState = HIGH;
-          digitalWrite(relayPin, HIGH);
-      }
+        globalTimer = 0;
+        shouldFlashLights = true;
+        return CommandSuccessful;
   }
   return CommandFailed;
 }
@@ -41,25 +49,72 @@ int setSantasMood(String command) {
   switch (moodOption) {
     // Happy - Green
     case 'H':
-      analogWrite(sadFaceLED, LOW);
-      analogWrite(mehFaceLED, LOW);
-      analogWrite(happyFaceLED, HIGH);
+      digitalWrite(sadFaceLED, LOW);
+      digitalWrite(mehFaceLED, LOW);
+      digitalWrite(happyFaceLED, HIGH);
       return CommandSuccessful;
     // Sad - Red
     case 'S':
-      analogWrite(happyFaceLED, LOW);
-      analogWrite(mehFaceLED, LOW);
-      analogWrite(sadFaceLED, HIGH);
+      digitalWrite(happyFaceLED, LOW);
+      digitalWrite(mehFaceLED, LOW);
+      digitalWrite(sadFaceLED, HIGH);
       return CommandSuccessful;
     // Meh - Yellow
     case 'M':
-      analogWrite(happyFaceLED, LOW);
-      analogWrite(sadFaceLED, LOW);
-      analogWrite(mehFaceLED, HIGH);
+      digitalWrite(happyFaceLED, LOW);
+      digitalWrite(sadFaceLED, LOW);
+      digitalWrite(mehFaceLED, HIGH);
+      return CommandSuccessful;
+    // None
+    case 'N':
+      digitalWrite(happyFaceLED, LOW);
+      digitalWrite(sadFaceLED, LOW);
+      digitalWrite(mehFaceLED, LOW);
       return CommandSuccessful;
   }
   return CommandFailed;
 }
 
+void setRelay(int value) {
+  relayState = value;
+  digitalWrite(relayPin, value);
+}
+
+void processChristmasTreeLights() {
+  switch (globalTimer) {
+    case 1:
+      setRelay(HIGH);
+      break;
+    case 2000:
+      setRelay(LOW);
+      break;
+    case 4000:
+      setRelay(HIGH);
+      break;
+    case 6000:
+      setRelay(LOW);
+      break;
+    case 8000:
+      setRelay(HIGH);
+      break;
+    case 10000:
+      setRelay(LOW);
+      break;
+    case 12000:
+      setRelay(HIGH);
+      break;
+    case 14000:
+      setRelay(LOW);
+      globalTimer = 0;
+      shouldFlashLights = false;
+      break;
+  }
+}
+
 void loop() {
+  Serial.println(globalTimer);
+  if (shouldFlashLights == true) {
+    globalTimer += 1;
+    processChristmasTreeLights();
+  }
 }
